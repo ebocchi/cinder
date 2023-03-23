18414f37448db92db93f49a338345636d0bdac90
Enrico Bocchi <enrico.bocchi@cern.ch>
2023-03-23 13:27:57 +0100

    Bypass deferred deletion for temporary volumes
    Fixes: https://bugs.launchpad.net/cinder/+bug/2012622

--- cinder/volume/drivers/rbd.py
+++ cinder/volume/drivers/rbd.py
@@ -1223,8 +1223,8 @@ class RBDDriver(driver.CloneableImageVD, driver.MigrateVD,
             @utils.retry(self.rbd.ImageBusy,
                          self.configuration.rados_connection_interval,
                          self.configuration.rados_connection_retries)
-            def _try_remove_volume(client, volume_name):
-                if self.configuration.enable_deferred_deletion:
+            def _try_remove_volume(client, volume_name, bypass_deferred_deletion=False):
+                if self.configuration.enable_deferred_deletion and not bypass_deferred_deletion:
                     LOG.debug("moving volume %s to trash", volume_name)
                     delay = self.configuration.deferred_deletion_delay
                     self.RBDProxy().trash_move(client.ioctx,
@@ -1236,7 +1236,14 @@ class RBDDriver(driver.CloneableImageVD, driver.MigrateVD,
             if clone_snap is None:
                 LOG.debug("deleting rbd volume %s", volume_name)
                 try:
-                    _try_remove_volume(client, volume_name)
+                    # NOTE(ebocchi): If the volume is temporary delete it immediately
+                    #                instead of moving to trash (deferred deletion).
+                    bypass_deferred_deletion = False
+                    with volume.obj_as_admin():
+                        if volume.admin_metadata.get('temporary', 'False') == 'True':
+                            LOG.info("bypassing deferred deletion for temp volume: %s", volume.id)
+                            bypass_deferred_deletion = True
+                    _try_remove_volume(client, volume_name, bypass_deferred_deletion)
                 except self.rbd.ImageBusy:
                     msg = (_("ImageBusy error raised while deleting rbd "
                              "volume. This may have been caused by a "
